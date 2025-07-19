abbb2375b012574d1d465bf36affc863f7a798de
thuctap-dungnt <tridung4323@gmail.com>
2025-07-20 03:25:42 +0700

    update

--- gym_be/gym_be/Models/GymContext.cs
+++ gym_be/gym_be/Models/GymContext.cs
@@ -18,6 +18,8 @@ namespace gym_be.Models
 
         public DbSet<WorkoutCourse> WorkoutCourses { get; set; }
 
+        public DbSet<Appointment> Appointments { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasDefaultSchema("public");
