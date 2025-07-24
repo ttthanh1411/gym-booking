bc2618c1e9ea649f17c778a8b8585688c26ddc5b
thuctap-dungnt <tridung4323@gmail.com>
2025-07-24 14:56:16 +0700

    update

--- gym_fe/src/service/serviceService.ts
+++ gym_fe/src/service/serviceService.ts
@@ -1,6 +1,25 @@
 import { Service } from '../type/service';
 import BaseService from './baseService';
 
-const serviceService = new BaseService<Service>('http://localhost:5000/api/service');
+class ServiceService extends BaseService<Service> {
+    constructor() {
+        super('https://localhost:5000/service');
+    }
 
+    // override getOne
+    async getOne(customerId: string): Promise<Service> {
+        const response = await fetch(`${this.baseUrl}/get?Serivceid=${customerId}`);
+        return this['handleResponse']<Service>(response); // gọi protected method từ cha
+    }
+
+    // override delete để dùng Customerid
+    async delete(serviceId: string): Promise<void> {
+        const response = await fetch(`${this.baseUrl}/delete?Serviceid=${serviceId}`, {
+            method: 'DELETE',
+        });
+        await this['handleResponse']<void>(response);
+    }
+}
+
+const serviceService = new ServiceService();
 export default serviceService;
